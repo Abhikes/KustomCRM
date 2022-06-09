53f2581d7dc905bf1be5619646f6d4e3ce40951c
saikr <saikr@LAPTOP-C09100BS>
2022-06-09 16:57:22 +0530

    BT Commit

--- Project/src/main/java/com_kustom/genericlib/BaseTest.java
+++ Project/src/main/java/com_kustom/genericlib/BaseTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 
-//Hello
+//Hello world
 public class BaseTest implements IAutoconsts {
 	public static WebDriver driver;
 	
