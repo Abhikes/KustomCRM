2632897724a1f5302f1ee665ba458cac808dbdfc
QSPJSP <QSPJSP@DESKTOP-QC4ES4V>
2022-06-16 10:27:42 +0530

    data_Demo

---
+++ MavenProjectDemo1/src/test/java/Demo/TestData.java
@@ -0,0 +1,14 @@
+package Demo;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestData {
+	@Test
+	public void m1()
+	{
+		Reporter.log("Hello");
+	}
+
+}
